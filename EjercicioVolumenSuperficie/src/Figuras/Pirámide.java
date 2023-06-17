d980da5ec725434922333cc6f8a01ce78cc5aead
ImPrinx <caritojago25@hotmail.com>
2023-06-16 19:24:13 -0500

    se agrega el proyecto

---
+++ EjercicioVolumenSuperficie/src/Figuras/Pirámide.java
@@ -0,0 +1,30 @@
+package Figuras;
+
+public class Pirámide extends FiguraGeométrica{
+    private double base; // Atributo que identifica la base de una pirámide 
+private double altura; // Atributo que identifica la altura de una pirámide
+private double apotema; /* Atributo que identifica el apotema de 
+una pirámide */
+
+public Pirámide(double base, double altura, double apotema) {
+this.base = base;
+this.altura = altura;
+this.apotema = apotema;
+this.setVolumen(calcularVolumen()); //Calcula el volumen y establece su atributo 
+this.setSuperficie(calcularSuperficie()); //Calcula la superficie y establece su atributo
+}
+
+public double calcularVolumen() {
+double volumen = (Math.pow(base, 2.0) * altura) / 3.0;
+return volumen;
+}
+
+public double calcularSuperficie() {
+double áreaBase = Math.pow(base, 2.0);
+double áreaLado = (base * apotema * 4)/2;
+return áreaBase + áreaLado;
+}
+}
+
+
+
---
+++ EjercicioVolumenSuperficie/src/Figuras/Principal.java
@@ -0,0 +1,19 @@
+package Figuras;
+
+public class Principal {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        VentanaPrincipal miVentanaPrincipal; /* Define la ventana 
+principal */
+miVentanaPrincipal= new VentanaPrincipal(); /* Crea la ventana 
+principal */
+miVentanaPrincipal.setVisible(true); /* Establece la ventana 
+como visible */
+// Establece que la ventana no puede cambiar su tamaño
+miVentanaPrincipal.setResizable(false);
+}
+}
+
+    
+    
+
